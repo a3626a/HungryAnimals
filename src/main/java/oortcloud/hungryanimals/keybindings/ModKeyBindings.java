95dbf6ab3dba0b2840d55828cb19eee6c45fb974
a3626a <a3626a@naver.com>
2015-08-14 14:50:08 +0900

    Fix Key Binding Bug

--- src/main/java/oortcloud/hungryanimals/keybindings/ModKeyBindings.java
+++ src/main/java/oortcloud/hungryanimals/keybindings/ModKeyBindings.java
@@ -7,8 +7,8 @@ public class ModKeyBindings {
 	
 	public static KeyBinding keyRight = new KeyBinding("increase value", 205, "Hungry Animals Debug");
 	public static KeyBinding keyLeft = new KeyBinding("decrease value", 203, "Hungry Animals Debug");
-	public static KeyBinding keyUp = new KeyBinding("change selection", 200, "Hungry Animals Debug");
-	public static KeyBinding keyDown = new KeyBinding("change selection", 208, "Hungry Animals Debug");
+	public static KeyBinding keyUp = new KeyBinding("change selection UP", 200, "Hungry Animals Debug");
+	public static KeyBinding keyDown = new KeyBinding("change selection DOWN", 208, "Hungry Animals Debug");
 	
 	public static void init() {
 		ClientRegistry.registerKeyBinding(keyRight);
