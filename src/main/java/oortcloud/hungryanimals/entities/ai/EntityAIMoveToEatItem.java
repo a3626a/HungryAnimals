1815e5ba0c6f367c4bdecee7f78d0b822be9fb53
a3626a <a3626a@naver.com>
2015-09-16 23:24:57 +0900

    Optimize and Improve Eat Fallen Food AI
    Integrate eat natural food and artificial food. eat closest food. eat
    when has more empty food value space than minimum food value of every
    edible food.

--- src/main/java/oortcloud/hungryanimals/entities/ai/EntityAIMoveToEatItem.java
+++ src/main/java/oortcloud/hungryanimals/entities/ai/EntityAIMoveToEatItem.java
@@ -1,8 +1,10 @@
 package oortcloud.hungryanimals.entities.ai;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 
 import net.minecraft.command.IEntitySelector;
 import net.minecraft.entity.Entity;
@@ -10,41 +12,118 @@ import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import oortcloud.hungryanimals.entities.properties.ExtendedPropertiesHungryAnimal;
 
 public class EntityAIMoveToEatItem extends EntityAIBase {
-	
+
 	private EntityLiving entity;
 	private ExtendedPropertiesHungryAnimal property;
 	private World worldObj;
-	private double xPosition;
-	private double yPosition;
-	private double zPosition;
 	private double speed;
-	private Predicate foodSelector;
 	private EntityItem target;
 
-	public EntityAIMoveToEatItem(EntityLiving entity, ExtendedPropertiesHungryAnimal prop, double speed) {
+	private double foodCondition = Double.MAX_VALUE;
+
+	private int delayCounter;
+
+	private Predicate EAT_EDIBLE = new Predicate() {
+		public boolean apply(EntityItem entityIn) {
+			return property.canEatFood(entityIn.getEntityItem());
+		}
+
+		@Override
+		public boolean apply(Object object) {
+			return this.apply((EntityItem) object);
+		}
+	};
+	private Predicate EAT_NATURAL = new Predicate() {
+		public boolean apply(EntityItem entityIn) {
+			ItemStack item = entityIn.getEntityItem();
+			NBTTagCompound tag = item.getTagCompound();
+			if (tag != null) {
+				return tag.hasKey("isNatural") && tag.getBoolean("isNatural");
+			}
+			return false;
+		}
+
+		@Override
+		public boolean apply(Object object) {
+			return this.apply((EntityItem) object);
+		}
+	};
+
+	public EntityAIMoveToEatItem(EntityLiving entity, ExtendedPropertiesHungryAnimal property, double speed) {
 		this.entity = entity;
-		this.property = prop;
+		this.property = property;
 		this.worldObj = this.entity.worldObj;
 		this.speed = speed;
-		foodSelector = new Predicate() {
-			public boolean apply(Entity entityIn) {
-				return property.canEatFood(((EntityItem) entityIn).getEntityItem());
-			}
-
-			@Override
-			public boolean apply(Object object) {
-				return this.apply((Entity) object);
-			}
-		};
 		this.setMutexBits(1);
 	}
 
-	public int executeProbability() {
+	@Override
+	public boolean shouldExecute() {
+		float radius = 16.0F;
+		initializeFoodCondition();
+		if (this.property.hunger_max - this.property.hunger < foodCondition)
+			return false;
+
+		if (this.delayCounter > 0) {
+			--this.delayCounter;
+			return false;
+		} else {
+
+			ArrayList<EntityItem> list;
+			list = (ArrayList<EntityItem>) this.worldObj.getEntitiesWithinAABB(EntityItem.class, entity.getEntityBoundingBox().expand(radius, radius, radius), Predicates.and(EAT_EDIBLE, EAT_NATURAL));
+			if (!list.isEmpty()) {
+				this.target = list.get(0);
+				return true;
+			}
+
+			if (entity.getRNG().nextInt(executeProbability()) != 0)
+				return false;
+
+			list = (ArrayList<EntityItem>) this.worldObj.getEntitiesWithinAABB(EntityItem.class, entity.getEntityBoundingBox().expand(radius, radius, radius), EAT_EDIBLE);
+			if (!list.isEmpty()) {
+				this.target = list.get(0);
+				return true;
+			}
+			return false;
+		}
+	}
+
+	@Override
+	public void startExecuting() {
+		this.entity.getNavigator().tryMoveToEntityLiving(target, speed);
+	}
+
+	@Override
+	public boolean continueExecuting() {
+		if (entity.getNavigator().noPath()) {
+			if (entity.getEntityBoundingBox().expand(1, 1, 1).isVecInside(target.getPositionVector())) {
+				ItemStack foodStack = target.getEntityItem();
+				foodStack.stackSize--;
+				if (foodStack.stackSize <= 0)
+					target.setDead();
+				this.property.eatFoodBonus(target.getEntityItem());
+			}
+			return false;
+		}
+		if (target.isDead)
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public void resetTask() {
+		this.target = null;
+		this.delayCounter = 100;
+	}
+
+	private int executeProbability() {
 		double taming = property.taming;
 		double hunger = property.getHungry();
 		if (taming > 1) {
@@ -53,73 +132,18 @@ public class EntityAIMoveToEatItem extends EntityAIBase {
 		if (taming > -1) {
 			taming = -1;
 		}
-
 		return (int) (200 * (taming - 1) * (taming - 1) * hunger) + 1;
 	}
 
 	/**
-	 * Returns whether the EntityAIBase should begin execution.
+	 * set "foodCondition" to minimum food value of edible foods.
 	 */
-	public boolean shouldExecute() {
-
-		if (!(this.property.getHungry() < this.property.courtship_hungerCondition && entity.getRNG().nextInt(executeProbability()) == 0))
-			return false;
-
-		List list = this.worldObj.getEntitiesWithinAABB(EntityItem.class, entity.getEntityBoundingBox().expand(10, 2, 10), foodSelector);
-
-		double maxhunger = 0;
-
-		if (list.isEmpty()) {
-			return false;
-		} else {
-			for (Object i : list) {
-				ItemStack item = ((EntityItem) i).getEntityItem();
-				if (property.getFoodHunger(item) > maxhunger) {
-					maxhunger = property.getFoodHunger(item);
-					this.target = (EntityItem) i;
-				}
-			}
-			if (this.target != null) {
-				this.xPosition = target.posX;
-				this.yPosition = target.posY;
-				this.zPosition = target.posZ;
-				return true;
-			} else {
-				return false;
+	private void initializeFoodCondition() {
+		if (foodCondition == Double.MAX_VALUE) {
+			for (double i : property.hunger_food.values()) {
+				if (i < foodCondition)
+					foodCondition = i;
 			}
 		}
-
 	}
-
-	/**
-	 * Returns whether an in-progress EntityAIBase should continue executing
-	 */
-	public boolean continueExecuting() {
-		if (entity.getNavigator().noPath()) {
-
-			if (entity.getEntityBoundingBox().expand(1, 1, 1).isVecInside(new Vec3(target.posX, target.posY, target.posZ))) {
-
-				ItemStack foodStack = target.getEntityItem();
-				foodStack.stackSize--;
-				if (foodStack.stackSize <= 0)
-					target.setDead();
-
-				this.property.eatFoodBonus(target.getEntityItem());
-			}
-
-			return false;
-		}
-		if (target == null)
-			return false;
-
-		return true;
-	}
-
-	/**
-	 * Execute a one shot task or start executing a continuous task
-	 */
-	public void startExecuting() {
-		entity.getNavigator().tryMoveToXYZ(this.xPosition, this.yPosition, this.zPosition, this.speed);
-	}
-
 }
