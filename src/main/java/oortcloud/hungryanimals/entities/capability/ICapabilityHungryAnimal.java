9b519b9d2fa9a7c13273266b688ec4159be47042
a3626a <a3626a@naver.com>
2016-12-31 17:58:40 +0900

    Capability design

--- src/main/java/oortcloud/hungryanimals/entities/capability/ICapabilityHungryAnimal.java
+++ src/main/java/oortcloud/hungryanimals/entities/capability/ICapabilityHungryAnimal.java
@@ -7,20 +7,11 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
 
 public interface ICapabilityHungryAnimal {
-	
-	public double getTaming();
-	public double setTaming(double taming);
-	
+		
 	public double getHunger();
 	public double setHunger(double hunger);
 	
 	public double getExcretion();
 	public double setExcretion(double excretion);
-
-	// ICapabiltityEventReciever
-	public void onUpdate();
-	public void onAttackedByPlayer(float damage, DamageSource source);
-	public boolean interact(EntityPlayer entity);
-	public void dropFewItems(boolean isHitByPlayer, int looting, List<EntityItem> drops);
 	
 }
---
+++ src/main/java/oortcloud/hungryanimals/entities/capability/ICapabilityTamableAnimal.java
@@ -0,0 +1,9 @@
+package oortcloud.hungryanimals.entities.capability;
+
+public class ICapabilityTamableAnimal {
+
+	public double getTaming();
+	public double setTaming(double taming);
+
+	
+}
