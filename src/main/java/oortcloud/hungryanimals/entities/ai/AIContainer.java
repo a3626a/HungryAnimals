947062961c477254e56372e9e27980818d610a17
Oortcloud <a3626a@naver.com>
2017-05-03 14:42:43 +0900

    Configuration to AIManager
    1. Map String to AIContainer
    ; each string corresponse to one ai container
    2. Inheritance of strings
    : can extend already written ai container. For example, pig will extend
    herbivore. WIP

--- src/main/java/oortcloud/hungryanimals/entities/ai/AIContainer.java
+++ src/main/java/oortcloud/hungryanimals/entities/ai/AIContainer.java
@@ -9,15 +9,16 @@ import net.minecraft.entity.ai.EntityAITasks.EntityAITaskEntry;
 import net.minecraft.entity.passive.EntityAnimal;
 
 public class AIContainer implements IAIContainer<EntityAnimal> {
-	
+
 	private LinkedList<IAIPlacer> ais;
 	private int start;
-	
+
 	private List<Class<? extends EntityAIBase>> toRemove;
-	
+	private boolean removeAll;
+
 	private List<Class<? extends EntityAIBase>> prior;
 	private List<Class<? extends EntityAIBase>> posterior;
-	
+
 	public AIContainer(int start) {
 		this.ais = new LinkedList<IAIPlacer>();
 		this.toRemove = new LinkedList<Class<? extends EntityAIBase>>();
@@ -26,162 +27,170 @@ public class AIContainer implements IAIContainer<EntityAnimal> {
 
 	@Override
 	public void registerAI(EntityAnimal entity) {
-		LinkedList<EntityAIBase> removeEntries = new LinkedList<EntityAIBase>();
-		for (EntityAITaskEntry i : entity.tasks.taskEntries) {
-			for (Class<? extends EntityAIBase> j : toRemove) {
-				if (i.action.getClass() == j) {
-					removeEntries.add(i.action);
+		if (removeAll) {
+			entity.tasks.taskEntries.clear();
+		} else {
+			LinkedList<EntityAIBase> removeEntries = new LinkedList<EntityAIBase>();
+			for (EntityAITaskEntry i : entity.tasks.taskEntries) {
+				for (Class<? extends EntityAIBase> j : toRemove) {
+					if (i.action.getClass() == j) {
+						removeEntries.add(i.action);
+					}
 				}
 			}
+			for (EntityAIBase i : removeEntries) {
+				entity.tasks.removeTask(i);
+			}
 		}
-		for (EntityAIBase i : removeEntries) {
-			entity.tasks.removeTask(i);
-		}
-		
+
 		List<EntityAIBase> aibases = new ArrayList<EntityAIBase>();
 		for (IAIPlacer i : ais) {
 			i.add(aibases, entity);
 		}
-		
+
 		int cnt = start;
 		for (EntityAIBase i : aibases) {
 			entity.tasks.addTask(cnt++, i);
 		}
 	}
-	
+
 	public void putFirst(AIFactory target) {
 		ais.add(new AIPlacerFirst(target));
 	}
-	
+
 	public void putLast(AIFactory target) {
 		ais.add(new AIPlacerLast(target));
 	}
-	
+
 	public void put(AIFactory target) {
 		ais.add(new AIPlacerPriority(target, prior, posterior));
 		prior = null;
 		posterior = null;
 	}
-	
+
 	public AIContainer priorTo(Class<? extends EntityAIBase> target) {
 		getPrior().add(target);
 		return this;
 	}
-	
+
 	public AIContainer priorTo(List<Class<? extends EntityAIBase>> targets) {
 		getPrior().addAll(targets);
 		return this;
 	}
-	
+
 	public AIContainer posteriorTo(Class<? extends EntityAIBase> target) {
 		getPosterior().add(target);
 		return this;
 	}
-	
+
 	public AIContainer posteriorTo(List<Class<? extends EntityAIBase>> targets) {
 		getPosterior().addAll(targets);
 		return this;
 	}
-	
+
 	private List<Class<? extends EntityAIBase>> getPrior() {
 		if (prior == null)
 			prior = new ArrayList<Class<? extends EntityAIBase>>();
 		return prior;
 	}
-	
+
 	private List<Class<? extends EntityAIBase>> getPosterior() {
 		if (posterior == null)
 			posterior = new ArrayList<Class<? extends EntityAIBase>>();
 		return posterior;
 	}
-	
+
 	public void remove(Class<? extends EntityAIBase> target) {
 		toRemove.add(target);
 	}
-	
+
 	public void remove(List<Class<? extends EntityAIBase>> target) {
 		toRemove.addAll(target);
 	}
-	
+
+	public void removeAll() {
+		removeAll = true;
+	}
+
 	@FunctionalInterface
 	public static interface AIFactory {
 		public EntityAIBase apply(EntityAnimal entity);
 	}
-	
+
 	private static interface IAIPlacer {
 		public boolean add(List<EntityAIBase> list, EntityAnimal entity);
 	}
-	
+
 	private static class AIPlacerFirst implements IAIPlacer {
 		public AIFactory aiFactory;
-		
+
 		public AIPlacerFirst(AIFactory aiFactory) {
 			this.aiFactory = aiFactory;
 		}
-		
+
 		@Override
 		public boolean add(List<EntityAIBase> list, EntityAnimal entity) {
 			list.add(0, aiFactory.apply(entity));
 			return true;
 		}
-		
+
 	}
-	
+
 	private static class AIPlacerLast implements IAIPlacer {
 		public AIFactory aiFactory;
-		
+
 		public AIPlacerLast(AIFactory aiFactory) {
 			this.aiFactory = aiFactory;
 		}
-		
+
 		@Override
 		public boolean add(List<EntityAIBase> list, EntityAnimal entity) {
 			list.add(aiFactory.apply(entity));
 			return true;
 		}
-		
+
 	}
-	
+
 	private static class AIPlacerPriority implements IAIPlacer {
-		
+
 		public AIFactory aiFactory;
 		public List<Class<? extends EntityAIBase>> prior;
 		public List<Class<? extends EntityAIBase>> posterior;
-		
+
 		public AIPlacerPriority(AIFactory aiFactory, List<Class<? extends EntityAIBase>> prior, List<Class<? extends EntityAIBase>> posterior) {
 			this.aiFactory = aiFactory;
 			this.prior = prior;
 			this.posterior = posterior;
 		}
-		
+
 		@Override
 		public boolean add(List<EntityAIBase> list, EntityAnimal entity) {
 			int maxIndex = list.size();
 			int minIndex = 0;
-			
+
 			for (Class<? extends EntityAIBase> i : prior) {
 				if (contains(list, i)) {
 					minIndex = Math.max(minIndex, indexOf(list, i));
 				}
 			}
-			
+
 			for (Class<? extends EntityAIBase> i : posterior) {
 				if (contains(list, i)) {
 					maxIndex = Math.min(minIndex, indexOf(list, i));
 				}
 			}
-			
+
 			prior = null;
 			posterior = null;
-			
-			if (minIndex<maxIndex) {
+
+			if (minIndex < maxIndex) {
 				list.add(maxIndex, aiFactory.apply(entity));
 				return true;
 			} else {
 				return false;
 			}
 		}
-		
+
 		private boolean contains(List<EntityAIBase> list, Class<? extends EntityAIBase> AIClass) {
 			for (EntityAIBase iAIBase : list) {
 				if (iAIBase.getClass() == AIClass)
@@ -189,7 +198,7 @@ public class AIContainer implements IAIContainer<EntityAnimal> {
 			}
 			return false;
 		}
-		
+
 		private int indexOf(List<EntityAIBase> list, Class<? extends EntityAIBase> aiClass) {
 			for (int i = 0; i < list.size(); i++) {
 				if (list.get(i).getClass() == aiClass) {
--- src/main/java/oortcloud/hungryanimals/entities/ai/AIManager.java
+++ src/main/java/oortcloud/hungryanimals/entities/ai/AIManager.java
@@ -2,6 +2,7 @@ package oortcloud.hungryanimals.entities.ai;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAIEatGrass;
@@ -9,6 +10,7 @@ import net.minecraft.entity.ai.EntityAIFollowParent;
 import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAIMate;
 import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAITempt;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
@@ -28,11 +30,13 @@ public class AIManager {
 	private static AIManager INSTANCE;
 
 	public Map<Class<? extends EntityAnimal>, IAIContainer<EntityAnimal>> REGISTRY;
+	public Map<String, Function<Class<? extends EntityAnimal>, IAIContainer<EntityAnimal>>> FACTORIES;
 
 	private AIManager() {
 		REGISTRY = new HashMap<Class<? extends EntityAnimal>, IAIContainer<EntityAnimal>>();
+		FACTORIES = new HashMap<String, Function<Class<? extends EntityAnimal>, IAIContainer<EntityAnimal>>>();
 	}
-	
+
 	public static AIManager getInstance() {
 		if (INSTANCE == null) {
 			INSTANCE = new AIManager();
@@ -41,130 +45,101 @@ public class AIManager {
 	}
 
 	public void init() {
-		AIContainer chicken = new AIContainer(1);
-		chicken.putLast((entity) -> new EntityAIAvoidPlayer(entity, 16.0F, 1.0D, 2.0D));
-		chicken.putLast((entity) -> new EntityAIMateModified(entity, 2.0D));
-		chicken.putLast((entity) -> new EntityAIMoveToTrough(entity, 1.0D));
-		chicken.putLast((entity) -> new EntityAITemptEdibleItem(entity, 1.5D, false));
-		chicken.putLast((entity) -> new EntityAIMoveToEatItem(entity, 1.5D));
-		chicken.putLast((entity) -> new EntityAIMoveToEatBlockChicken(entity, 1.0D));
-		chicken.putLast((entity) -> new EntityAIWander(entity, 1.0D));
-		chicken.putLast((entity) -> new EntityAIWatchClosest(entity, EntityPlayer.class, 6.0F));
-		chicken.putLast((entity) ->  new EntityAILookIdle(entity));
-		chicken.remove(EntityAITempt.class);
-		chicken.remove(EntityAIFollowParent.class);
-		chicken.remove(EntityAIWander.class);
-		chicken.remove(EntityAIMate.class);
-		chicken.remove(EntityAIPanic.class);
-		chicken.remove(EntityAIWatchClosest.class);
-		chicken.remove(EntityAILookIdle.class);
+		FACTORIES.put("herbivore", (animal) -> {
+			AIContainer aiContainer = new AIContainer(0);
+			aiContainer.putLast((entity) -> new EntityAISwimming(entity ));
+			aiContainer.putLast((entity) -> new EntityAIAvoidPlayer(entity, 16.0F, 1.0D, 2.0D));
+			aiContainer.putLast((entity) -> new EntityAIMateModified(entity, 2.0D));
+			aiContainer.putLast((entity) -> new EntityAIMoveToTrough(entity, 1.0D));
+			aiContainer.putLast((entity) -> new EntityAITemptEdibleItem(entity, 1.5D, false));
+			aiContainer.putLast((entity) -> new EntityAIMoveToEatItem(entity, 1.5D));
+			aiContainer.putLast((entity) -> new EntityAIMoveToEatBlock(entity, 1.0D));
+			aiContainer.putLast((entity) -> new EntityAIWander(entity, 1.0D));
+			aiContainer.putLast((entity) -> new EntityAIWatchClosest(entity, EntityPlayer.class, 6.0F));
+			aiContainer.putLast((entity) -> new EntityAILookIdle(entity));
+			aiContainer.removeAll();
+			return aiContainer;
+		});
+		// TODO After JSON load
 		MinecraftForge.EVENT_BUS.post(new AIContainerRegisterEvent(EntityChicken.class, chicken));
 		REGISTRY.put(EntityChicken.class, chicken);
-
-		AIContainer cow = new AIContainer(1);
-		cow.putLast((entity) -> new EntityAIAvoidPlayer(entity, 16.0F, 1.0D, 2.0D));
-		cow.putLast((entity) -> new EntityAIMateModified(entity, 2.0D));
-		cow.putLast((entity) -> new EntityAIMoveToTrough(entity, 1.0D));
-		cow.putLast((entity) -> new EntityAITemptEdibleItem(entity, 1.5D, false));
-		cow.putLast((entity) -> new EntityAIMoveToEatItem(entity, 1.5D));
-		cow.putLast((entity) -> new EntityAIMoveToEatBlock(entity, 1.0D));
-		cow.putLast((entity) -> new EntityAIWander(entity, 1.0D));
-		cow.putLast((entity) -> new EntityAIWatchClosest(entity, EntityPlayer.class, 6.0F));
-		cow.putLast((entity) ->  new EntityAILookIdle(entity));
-		cow.remove(EntityAITempt.class);
-		cow.remove(EntityAIFollowParent.class);
-		cow.remove(EntityAIWander.class);
-		cow.remove(EntityAIMate.class);
-		cow.remove(EntityAIPanic.class);
-		cow.remove(EntityAIWatchClosest.class);
-		cow.remove(EntityAILookIdle.class);
 		MinecraftForge.EVENT_BUS.post(new AIContainerRegisterEvent(EntityCow.class, cow));
 		REGISTRY.put(EntityCow.class, cow);
-		
-		AIContainer mooshroom = new AIContainer(1);
-		mooshroom.putLast((entity) -> new EntityAIAvoidPlayer(entity, 16.0F, 1.0D, 2.0D));
-		mooshroom.putLast((entity) -> new EntityAIMateModified(entity, 2.0D));
-		mooshroom.putLast((entity) -> new EntityAIMoveToTrough(entity, 1.0D));
-		mooshroom.putLast((entity) -> new EntityAITemptEdibleItem(entity, 1.5D, false));
-		mooshroom.putLast((entity) -> new EntityAIMoveToEatItem(entity, 1.5D));
-		mooshroom.putLast((entity) -> new EntityAIMoveToEatBlock(entity, 1.0D));
-		mooshroom.putLast((entity) -> new EntityAIWander(entity, 1.0D));
-		mooshroom.putLast((entity) -> new EntityAIWatchClosest(entity, EntityPlayer.class, 6.0F));
-		mooshroom.putLast((entity) ->  new EntityAILookIdle(entity));
-		mooshroom.remove(EntityAITempt.class);
-		mooshroom.remove(EntityAIFollowParent.class);
-		mooshroom.remove(EntityAIWander.class);
-		mooshroom.remove(EntityAIMate.class);
-		mooshroom.remove(EntityAIPanic.class);
-		mooshroom.remove(EntityAIWatchClosest.class);
-		mooshroom.remove(EntityAILookIdle.class);
 		MinecraftForge.EVENT_BUS.post(new AIContainerRegisterEvent(EntityMooshroom.class, mooshroom));
 		REGISTRY.put(EntityMooshroom.class, mooshroom);
-		
-		AIContainer pig = new AIContainer(1);
-		pig.putLast((entity) -> new EntityAIAvoidPlayer(entity, 16.0F, 1.0D, 2.0D));
-		pig.putLast((entity) -> new EntityAIMateModified(entity, 2.0D));
-		pig.putLast((entity) -> new EntityAIMoveToTrough(entity, 1.0D));
-		pig.putLast((entity) -> new EntityAITempt(entity, 1.5D, Items.CARROT_ON_A_STICK, false));
-		pig.putLast((entity) -> new EntityAITemptEdibleItem(entity, 1.5D, false));
-		pig.putLast((entity) -> new EntityAIMoveToEatItem(entity, 1.5D));
-		pig.putLast((entity) -> new EntityAIMoveToEatBlock(entity, 1.0D));
-		pig.putLast((entity) -> new EntityAIWander(entity, 1.0D));
-		pig.putLast((entity) -> new EntityAIWatchClosest(entity, EntityPlayer.class, 6.0F));
-		pig.putLast((entity) ->  new EntityAILookIdle(entity));
-		pig.remove(EntityAITempt.class);
-		pig.remove(EntityAIFollowParent.class);
-		pig.remove(EntityAIWander.class);
-		pig.remove(EntityAIMate.class);
-		pig.remove(EntityAIPanic.class);
-		pig.remove(EntityAIWatchClosest.class);
-		pig.remove(EntityAILookIdle.class);
+
+		FACTORIES.put("pig", (animal) -> {
+			AIContainer aiContainer = new AIContainer(1);
+			aiContainer.putLast((entity) -> new EntityAIAvoidPlayer(entity, 16.0F, 1.0D, 2.0D));
+			aiContainer.putLast((entity) -> new EntityAIMateModified(entity, 2.0D));
+			aiContainer.putLast((entity) -> new EntityAIMoveToTrough(entity, 1.0D));
+			aiContainer.putLast((entity) -> new EntityAITempt(entity, 1.5D, Items.CARROT_ON_A_STICK, false));
+			aiContainer.putLast((entity) -> new EntityAITemptEdibleItem(entity, 1.5D, false));
+			aiContainer.putLast((entity) -> new EntityAIMoveToEatItem(entity, 1.5D));
+			aiContainer.putLast((entity) -> new EntityAIMoveToEatBlock(entity, 1.0D));
+			aiContainer.putLast((entity) -> new EntityAIWander(entity, 1.0D));
+			aiContainer.putLast((entity) -> new EntityAIWatchClosest(entity, EntityPlayer.class, 6.0F));
+			aiContainer.putLast((entity) -> new EntityAILookIdle(entity));
+			aiContainer.remove(EntityAITempt.class);
+			aiContainer.remove(EntityAIFollowParent.class);
+			aiContainer.remove(EntityAIWander.class);
+			aiContainer.remove(EntityAIMate.class);
+			aiContainer.remove(EntityAIPanic.class);
+			aiContainer.remove(EntityAIWatchClosest.class);
+			aiContainer.remove(EntityAILookIdle.class);
+			return aiContainer;
+		});
 		MinecraftForge.EVENT_BUS.post(new AIContainerRegisterEvent(EntityPig.class, pig));
 		REGISTRY.put(EntityPig.class, pig);
 
-		AIContainer rabbit = new AIContainer(1);
-		rabbit.putLast((entity) -> new EntityAIAvoidPlayer(entity, 16.0F, 1.0D, 2.0D));
-		rabbit.putLast((entity) -> new EntityAIMateModified(entity, 2.0D));
-		rabbit.putLast((entity) -> new EntityAIMoveToTrough(entity, 1.0D));
-		rabbit.putLast((entity) -> new EntityAITempt(entity, 1.5D, Items.CARROT_ON_A_STICK, false));
-		rabbit.putLast((entity) -> new EntityAITemptEdibleItem(entity, 1.5D, false));
-		rabbit.putLast((entity) -> new EntityAIMoveToEatItem(entity, 1.5D));
-		rabbit.putLast((entity) -> new EntityAIMoveToEatBlock(entity, 1.0D));
-		rabbit.putLast((entity) -> new EntityAIWander(entity, 1.0D));
-		rabbit.putLast((entity) -> new EntityAIWatchClosest(entity, EntityPlayer.class, 6.0F));
-		rabbit.putLast((entity) ->  new EntityAILookIdle(entity));
-		for (Class<?> i : EntityRabbit.class.getDeclaredClasses()) {
-			if (i.isAssignableFrom(EntityAIBase.class))
-				rabbit.remove(i.asSubclass(EntityAIBase.class));
-		}
-		rabbit.remove(EntityAITempt.class);
-		rabbit.remove(EntityAIFollowParent.class);
-		rabbit.remove(EntityAIWander.class);
-		rabbit.remove(EntityAIMate.class);
-		rabbit.remove(EntityAIPanic.class);
-		rabbit.remove(EntityAIWatchClosest.class);
-		rabbit.remove(EntityAILookIdle.class);
+		FACTORIES.put("rabbit", (animal) -> {
+			AIContainer aiContainer = new AIContainer(1);
+			aiContainer.putLast((entity) -> new EntityAIAvoidPlayer(entity, 16.0F, 1.0D, 2.0D));
+			aiContainer.putLast((entity) -> new EntityAIMateModified(entity, 2.0D));
+			aiContainer.putLast((entity) -> new EntityAIMoveToTrough(entity, 1.0D));
+			aiContainer.putLast((entity) -> new EntityAITemptEdibleItem(entity, 1.5D, false));
+			aiContainer.putLast((entity) -> new EntityAIMoveToEatItem(entity, 1.5D));
+			aiContainer.putLast((entity) -> new EntityAIMoveToEatBlock(entity, 1.0D));
+			aiContainer.putLast((entity) -> new EntityAIWander(entity, 1.0D));
+			aiContainer.putLast((entity) -> new EntityAIWatchClosest(entity, EntityPlayer.class, 6.0F));
+			aiContainer.putLast((entity) -> new EntityAILookIdle(entity));
+			for (Class<?> i : EntityRabbit.class.getDeclaredClasses()) {
+				if (i.isAssignableFrom(EntityAIBase.class))
+					aiContainer.remove(i.asSubclass(EntityAIBase.class));
+			}
+			aiContainer.remove(EntityAITempt.class);
+			aiContainer.remove(EntityAIFollowParent.class);
+			aiContainer.remove(EntityAIWander.class);
+			aiContainer.remove(EntityAIMate.class);
+			aiContainer.remove(EntityAIPanic.class);
+			aiContainer.remove(EntityAIWatchClosest.class);
+			aiContainer.remove(EntityAILookIdle.class);
+			return aiContainer;
+		});
 		MinecraftForge.EVENT_BUS.post(new AIContainerRegisterEvent(EntityRabbit.class, rabbit));
 		REGISTRY.put(EntityRabbit.class, rabbit);
-		
-		AIContainer sheep = new AIContainer(1);
-		sheep.putLast((entity) -> new EntityAIAvoidPlayer(entity, 16.0F, 1.0D, 2.0D));
-		sheep.putLast((entity) -> new EntityAIMateModified(entity, 2.0D));
-		sheep.putLast((entity) -> new EntityAIMoveToTrough(entity, 1.0D));
-		sheep.putLast((entity) -> new EntityAITemptEdibleItem(entity, 1.5D, false));
-		sheep.putLast((entity) -> new EntityAIMoveToEatItem(entity, 1.5D));
-		sheep.putLast((entity) -> new EntityAIMoveToEatBlock(entity, 1.0D));
-		sheep.putLast((entity) -> new EntityAIWander(entity, 1.0D));
-		sheep.putLast((entity) -> new EntityAIWatchClosest(entity, EntityPlayer.class, 6.0F));
-		sheep.putLast((entity) ->  new EntityAILookIdle(entity));
-		sheep.remove(EntityAITempt.class);
-		sheep.remove(EntityAIFollowParent.class);
-		sheep.remove(EntityAIWander.class);
-		sheep.remove(EntityAIMate.class);
-		sheep.remove(EntityAIPanic.class);
-		sheep.remove(EntityAIWatchClosest.class);
-		sheep.remove(EntityAILookIdle.class);
-		sheep.remove(EntityAIEatGrass.class);
+
+		FACTORIES.put("sheep", (animal) -> {
+			AIContainer aiContainer = new AIContainer(1);
+			aiContainer.putLast((entity) -> new EntityAIAvoidPlayer(entity, 16.0F, 1.0D, 2.0D));
+			aiContainer.putLast((entity) -> new EntityAIMateModified(entity, 2.0D));
+			aiContainer.putLast((entity) -> new EntityAIMoveToTrough(entity, 1.0D));
+			aiContainer.putLast((entity) -> new EntityAITemptEdibleItem(entity, 1.5D, false));
+			aiContainer.putLast((entity) -> new EntityAIMoveToEatItem(entity, 1.5D));
+			aiContainer.putLast((entity) -> new EntityAIMoveToEatBlock(entity, 1.0D));
+			aiContainer.putLast((entity) -> new EntityAIWander(entity, 1.0D));
+			aiContainer.putLast((entity) -> new EntityAIWatchClosest(entity, EntityPlayer.class, 6.0F));
+			aiContainer.putLast((entity) -> new EntityAILookIdle(entity));
+			aiContainer.remove(EntityAITempt.class);
+			aiContainer.remove(EntityAIFollowParent.class);
+			aiContainer.remove(EntityAIWander.class);
+			aiContainer.remove(EntityAIMate.class);
+			aiContainer.remove(EntityAIPanic.class);
+			aiContainer.remove(EntityAIWatchClosest.class);
+			aiContainer.remove(EntityAILookIdle.class);
+			aiContainer.remove(EntityAIEatGrass.class);
+			return aiContainer;
+		});
 		MinecraftForge.EVENT_BUS.post(new AIContainerRegisterEvent(EntitySheep.class, sheep));
 		REGISTRY.put(EntitySheep.class, sheep);
 	}
